b90085fb0bc0827a9314969ad24a5e84dc296a95
Eric Bernstein <e.bernstein@sap.com>
2020-02-18 19:09:02 +0100

    Read files and save in variables

--- practise-round/orderPizza.py
+++ practise-round/orderPizza.py
@@ -21,9 +21,62 @@ def orderPizzas():
         with open(INPUT_DATA_FOLDER + SEP + fileBase + INPUT_DATA_EXT, "r") as f:
             outputFile = OUTPUT_DATA_DIR + SEP + fileBase + OUTPUT_DATA_EXT
 
+            # print(f.readlines())
+            getBestPizzaOrder(inputLines = f.readlines())
             outputData = ""
 
             saveOutput(outputFile, outputData)
+
+
+def getBestPizzaOrder(inputLines):
+    firstLine = True
+    maxSlices = 0               # maximum slices to order
+    difPizzas = 0               # number of different pizzas available == len(numberOfSlicesPerPizza)
+    numberOfSlicesPerPizza = [] # list with the number of slices per pizza
+
+    for line in inputLines:
+        line = line.strip('\n')
+        numberList = line.split(' ')
+        if(firstLine):
+           maxSlices = int(numberList[0])
+           difPizzas = int(numberList[1])
+           firstLine = False
+        else:
+            numberOfSlicesPerPizza = numberList
+
+    sortFromLeftToRight(maximumSlices=maxSlices, differentPizzas=difPizzas, slicesPerPizza=numberOfSlicesPerPizza)
+
+    return
+
+
+def sortFromLeftToRight(maximumSlices: int, differentPizzas: int, slicesPerPizza: list):
+    maximumSliceSum = 0
+    pizzaSelection = differentPizzas * '0'
+
+    i = -1
+    currentSliceSum = 0
+    lastSliceNumberAdded = 0
+
+    for sliceNumber in slicesPerPizza:
+        sliceNumber = int(sliceNumber)
+        i += 1
+
+        if(currentSliceSum < maximumSlices):
+            lastSliceNumberAdded = sliceNumber
+            currentSliceSum += sliceNumber
+            pizzaSelection = pizzaSelection[:i] + '1' + pizzaSelection[i + 1:] # replace the 0 in the selectionString with a 1
+
+        else:
+            print(pizzaSelection)
+            maximumSliceSum = currentSliceSum - lastSliceNumberAdded
+            print(maximumSliceSum)
+            return
+
+        #print(sliceNumber)
+
+    print(pizzaSelection)
+    
+    return
 
 
 def saveOutput(fileLocation, data):
