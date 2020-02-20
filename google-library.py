b518260ce4e250a1f26ae05498c8c808f58b49e2
Luca Bernstein <luca@git.lucabernstein.com>
2020-02-20 19:02:16 +0100

    Fill with stub content

---
+++ google-library.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+SEP = "/"
+INPUT_DATA_FOLDER = "input-data-sets"
+INPUT_DATA_FILES = [
+    "a_example",
+    "b_small",
+    "c_medium",
+    "d_quite_big",
+    "e_also_big"
+]
+INPUT_DATA_EXT = ".in"
+OUTPUT_DATA_DIR = "out"
+OUTPUT_DATA_EXT = ".out"
+
+def scanBooks():
+    pass
+
+if __name__ == "__main__":
+    scanBooks()
