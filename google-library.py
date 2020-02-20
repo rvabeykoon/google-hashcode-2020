6d254b525ff5aa550d7545fd54634f26fa1bb7b4
Luca Bernstein <luca@git.lucabernstein.com>
2020-02-20 19:06:53 +0100

    Load books

--- google-library.py
+++ google-library.py
@@ -1,20 +1,25 @@
 #!/bin/python3
 
 SEP = "/"
-INPUT_DATA_FOLDER = "input-data-sets"
+INPUT_DATA_FOLDER = "library-in-files"
 INPUT_DATA_FILES = [
     "a_example",
-    "b_small",
-    "c_medium",
-    "d_quite_big",
-    "e_also_big"
+    #"b_read_on",
+    #"c_incunabula",
+    #"d_tough_choices",
+    #"e_so_many_books",
+    #"f_libraries_of_the_world"
 ]
-INPUT_DATA_EXT = ".in"
+INPUT_DATA_EXT = ".txt"
 OUTPUT_DATA_DIR = "out"
 OUTPUT_DATA_EXT = ".out"
 
 def scanBooks():
-    pass
+    for fileBase in INPUT_DATA_FILES:
+        with open(INPUT_DATA_FOLDER + SEP + fileBase + INPUT_DATA_EXT, "r") as f:
+            outputFile = OUTPUT_DATA_DIR + SEP + fileBase + OUTPUT_DATA_EXT
+
+
 
 if __name__ == "__main__":
     scanBooks()
